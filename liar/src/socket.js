e5175028ebd11029f9acf1dc52b72c638075e82b
Olivier Gabison <olivier@Oliviers-MacBook-Pro-2.local>
2023-05-17 02:06:24 -0400

    url fix

--- liar/src/socket.js
+++ liar/src/socket.js
@@ -1,6 +1,9 @@
-import { io } from 'socket.io-client';
+import { io } from "socket.io-client";
 
 // "undefined" means the URL will be computed from the `window.location` object
-const URL = process.env.NODE_ENV === 'production' ? undefined : 'http://localhost:4000';
+const URL =
+  process.env.NODE_ENV === "production"
+    ? "https://secret-falls-26151.herokuapp.com"
+    : "http://localhost:4000";
 
 export const socket = io(URL);
